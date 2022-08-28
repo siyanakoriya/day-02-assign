aa03f5f0d061c55c5cf4a289c5ae3591577f9f2e
siyanakoriya <siyanakoriya122@gmail.com>
2022-08-28 19:43:15 +0530

    n natural number

---
+++ src/Nnatural.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Nnatural {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int a = sc.nextInt();
+        int sum=0;
+        int i =1;
+        while (i<=a){
+            sum +=i;
+            i++;
+        }
+        System.out.println("The result is: "+sum);
+    }
+}
