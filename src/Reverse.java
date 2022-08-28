a09641546e30c26c7338acc8af5bb335bc6a3598
siyanakoriya <siyanakoriya122@gmail.com>
2022-08-28 19:38:54 +0530

    Reverse while loop

---
+++ src/Reverse.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+        int n, r;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        n = sc.nextInt();
+        while (n > 0) {
+            r = n % 10;
+            System.out.print(r);
+            n = n / 10;
+
+        }
+    }
+
+}
