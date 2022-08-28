69b8f702ea4370c9fd9c84ba804bf92068810158
siyanakoriya <siyanakoriya122@gmail.com>
2022-08-28 14:30:54 +0530

    Palidrome number

---
+++ src/PalindromeFor.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class PalindromeFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int n = sc.nextInt();
+        int c = n;
+        int r = 0;
+        int s = 0;
+        for (; n > 0; ) {
+            r = n % 10;
+            s = s * 10 + r;
+            n = n / 10;
+        }
+        if (c == s)
+            System.out.println("Entered number is a palindrome.");
+        else
+
+            System.out.println("Entered number is not a palindrome.");
+
+    }
+}
