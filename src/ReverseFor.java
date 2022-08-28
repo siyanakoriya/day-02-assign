40de1874248cf71f8640d7d8fc48aa3e29b974ac
siyanakoriya <siyanakoriya122@gmail.com>
2022-08-28 14:28:20 +0530

    Reverse for integer number

---
+++ src/ReverseFor.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+
+public class ReverseFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int n = sc.nextInt();
+
+        int r = 0;
+        for (; n > 0; ) {
+            r = n % 10;
+            n = n / 10;
+
+            System.out.print(r);
+        }
+    }
+}
